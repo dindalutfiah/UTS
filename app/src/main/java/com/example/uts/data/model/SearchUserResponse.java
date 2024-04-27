ae83cc56fd8840745d878c5ee14d868ce79b2882
dindalutfiah <12250125077@students.uin-suska.ac.id>
2024-04-27 12:09:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts/data/model/SearchUserResponse.java
@@ -0,0 +1,16 @@
+package com.example.uts.data.model;
+
+import java.util.List;
+
+public class SearchUserResponse {
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
+
